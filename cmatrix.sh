0f8534f2dbd89d365e2929c236e4c27b427dd5e4
Luke Crook <luke@balooga.com>
2017-12-19 14:47:47 -0800

    CMatrix now hosted on GitHub

--- cmatrix.sh
+++ cmatrix.sh
@@ -1,13 +1,13 @@
 if [ ! -d ~/.vagrant-installation/cmatrix ] 
 then
     echo Installing cMatrix
-
-    mkdir ~/.vagrant-installation/cmatrix
+	cd ~/.vagrant-installation/
+	git clone https://github.com/abishekvashok/cmatrix.git
     cd ~/.vagrant-installation/cmatrix
 
-    wget https://www.asty.org/cmatrix/dist/cmatrix-1.2a.tar.gz
-    tar xvzf cmatrix-1.2a.tar.gz
-    cd cmatrix-1.2a
+    #wget https://www.asty.org/cmatrix/dist/cmatrix-1.2a.tar.gz
+    #tar xvzf cmatrix-1.2a.tar.gz
+    #cd cmatrix-1.2a
 
     aclocal
     autoconf
