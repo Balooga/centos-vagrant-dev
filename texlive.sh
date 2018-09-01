15c56b7bc649f99112ecad80ec28284317132544
Luke Crook <luke@balooga.com>
2018-08-31 17:26:26 -0700

    Add support for TexLive 2018

--- texlive.sh
+++ texlive.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # To remove a previous installation
-# rm -rf /usr/local/texlive/2016
-# rm -rf ~/.texlive2016
+# rm -rf /usr/local/texlive/2018
+# New installations of texlive require a new texlive.profile
+# which is created following a successful install of texlive.
+# texlive.profile can be found in /usr/local/texlive/2018/tlpkg/texlive.profile
 
-if [ ! -d /usr/local/texlive/2017 ]
+if [ ! -d /usr/local/texlive/2018 ]
 then
     echo Downloading TexLive.
     wget -q -c --progress=dot:giga http://muug.ca/mirror/ctan/systems/texlive/Images/texlive.iso
@@ -12,9 +14,6 @@ then
     echo Installing TexLive.
     mount -t iso9660 -o ro,loop,noauto texlive.iso /mnt
 
-    # New installations of texlive require a new texlive.profile
-    # which is created following a successful install of texlive.
-    # texlive.profile can be found in /usr/local/texlive/2017/tlpkg/texlive.profile
     cd /mnt
     ./install-tl -profile /home/vagrant/texlive.profile
 
@@ -23,33 +22,36 @@ then
     rm /home/vagrant/texlive.iso
     rm /home/vagrant/texlive.profile
 
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr path add
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install graphics-def
-
-    echo Updating TexLive.
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet update --self
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet update --all
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr path add
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install graphics-def
 
     echo Install Sphinx-Doc style dependencies
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install tabulary
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install fncychap
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install framed
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install threeparttable
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install wrapfig
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install capt-of
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install multirow
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install eqparbox
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install environ
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install trimspaces
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install tabulary
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install fncychap
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install framed
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install threeparttable
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install wrapfig
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install capt-of
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install multirow
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install eqparbox
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install environ
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install trimspaces
 
     echo And latex fonts....
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install collection-fontsrecommended
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install collection-fontsextra
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install collection-fontsrecommended
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install collection-fontsextra
 
-    /usr/local/texlive/2017/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install epstopdf
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet install epstopdf
 
-    echo 'PATH=$PATH:/usr/local/texlive/2017/bin/x86_64-linux' >> /home/vagrant/.bash_profile
+    echo 'PATH=$PATH:/usr/local/texlive/2018/bin/x86_64-linux' >> /home/vagrant/.bash_profile
     echo export PATH >> /home/vagrant/.bash_profile
 
     echo TexLive Installation Complete.
+fi
+
+if [ -d /usr/local/texlive/2018 ]
+then
+    echo Updating TexLive.
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet update --self
+    /usr/local/texlive/2018/bin/x86_64-linux/tlmgr -repository http://mirror.ctan.org/systems/texlive/tlnet update --all
 fi
