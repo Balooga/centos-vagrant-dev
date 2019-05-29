d5672cce594c3d9a7c430832d582deac26159e1f
Luke Crook <luke.crook@nuorder.com>
2019-05-29 12:13:59 -0700

    Changes for Go.

--- golang.sh
+++ golang.sh
@@ -28,7 +28,23 @@ then
     /usr/local/go/bin/go get -u -v golang.org/x/tools/cmd/guru
     /usr/local/go/bin/go get -u -v golang.org/x/tools/cmd/gorename
     /usr/local/go/bin/go get -u -v golang.org/x/tools/cmd/goimports
-    /usr/local/go/bin/go get -u -v github.com/alecthomas/gometalinter
+    # /usr/local/go/bin/go get -u -v github.com/alecthomas/gometalinter
+    /usr/local/go/bin/go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
+    /usr/local/go/bin/go get -u -v golang.org/x/tools/cmd/godoc
+    /usr/local/go/bin/go get -u -v github.com/zmb3/gogetdoc
+    /usr/local/go/bin/go get -u -v github.com/cweill/gotests/...
+    /usr/local/go/bin/go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
+    /usr/local/go/bin/go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+    /usr/local/go/bin/go get -u -v github.com/josharian/impl
+    /usr/local/go/bin/go get -u -v github.com/fatih/gomodifytags
+    /usr/local/go/bin/go get -u -v github.com/mdempsky/unconvert
+    /usr/local/go/bin/go get -u -v gopkg.in/check.v1
+    /usr/local/go/bin/go get -u -v github.com/kisielk/errcheck
+    /usr/local/go/bin/go get -u golang.org/x/lint/golint
+
+    /usr/local/go/bin/go get -u -v github.com/godoctor/godoctor
+    /usr/local/go/bin/go install github.com/godoctor/godoctor
+
     ~/go/bin/gometalinter --install --update
     cd ~/
 fi
