b1d2bd54efb49ba273f856991e892f726a956571
Luke Crook <luke.crook@nuorder.com>
2019-07-16 18:15:03 -0700

    Modified PATH exports

--- paths.sh
+++ paths.sh
@@ -2,26 +2,29 @@
 
 if [ -d $HOME/.local/bin ]
 then
-    echo 'export PATH=$PATH:$HOME/.local/bin' >> /home/vagrant/.bashrc
+    echo 'PATH=$PATH:$HOME/.local/bin' >> /home/vagrant/.bashrc
 fi
 if [ -d $HOME/bin ]
 then
-    echo 'export PATH=$PATH:$HOME/bin' >> /home/vagrant/.bashrc
+    echo 'PATH=$PATH:$HOME/bin' >> /home/vagrant/.bashrc
 fi
 
 if [ -d /usr/local/go/bin ]
 then
-   echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/vagrant/.bashrc
+   echo 'PATH=$PATH:/usr/local/go/bin' >> /home/vagrant/.bashrc
 fi
 if [ -d $HOME/go ]
 then
-   echo 'export GOPATH=$HOME/go' >> /home/vagrant/.bashrc
+   echo 'GOPATH=$HOME/go' >> /home/vagrant/.bashrc
 fi
 if [ -d ${GOPATH//://bin:}/bin ]
 then
-   echo 'export PATH=$PATH:${GOPATH//://bin:}/bin' >> /home/vagrant/.bashrc
+   echo 'PATH=$PATH:${GOPATH//://bin:}/bin' >> /home/vagrant/.bashrc
 fi
 if [ -d /usr/local/texlive/2019/bin/x86_64-linux ]
 then
-    echo 'export PATH=$PATH:/usr/local/texlive/2019/bin/x86_64-linux' >> /home/vagrant/.bashrc
+    echo 'PATH=$PATH:/usr/local/texlive/2019/bin/x86_64-linux' >> /home/vagrant/.bashrc
 fi
+
+echo 'export GOPATH' >> /home/vagrant/.bashrc
+echo 'export PATH' >> /home/vagrant/.bashrc
